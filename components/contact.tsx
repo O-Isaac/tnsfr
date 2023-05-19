539b7febfac561d6a0c485bb8b689f7d11c1f590
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-05-19 22:16:37 +0200

    feat(contact): Add Modal Feedback

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,7 @@
 import { ContactModalContext } from "@/context/showContact"
-import { Modal, TextInput, Label, Textarea, Button } from "flowbite-react"
-import { FormEvent, useContext } from "react"
-import { HiMail, HiAnnotation } from "react-icons/hi"
+import { Modal, TextInput, Label, Textarea, Button, Alert } from "flowbite-react"
+import { FormEvent, useContext, useState } from "react"
+import { HiMail, HiAnnotation, HiInformationCircle } from "react-icons/hi"
 
 interface Prop {
     show: boolean
@@ -9,6 +9,14 @@ interface Prop {
 
 export default function ModalContact(props: Prop) {
     const { setShow } = useContext(ContactModalContext)
+    const [ success, setSuccess ] = useState<boolean>(false)
+    const [ error, setError] = useState<boolean>(false)
+
+    const onCloseModal = () => {
+        if (setShow) setShow(false)
+        setSuccess(false)
+        setError(false)
+    }
 
     const handleSumbit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -22,14 +30,20 @@ export default function ModalContact(props: Prop) {
         const request = await fetch("/api/contact", options)
         const response = await request.json()
 
-        console.log(response)
+        if (response.status !== 200) {
+            setError(true)
+            if (success) setSuccess(false)
+        } else {
+            setSuccess(true)
+            if (error) setError(true)
+        }
     }
 
     return (
-        <Modal show={props.show} onClose={() => setShow && setShow(false)} size="lg" popup={true}>
-            <Modal.Header />
+        <Modal show={props.show} onClose={onCloseModal} size="xl" popup={true}>
+            <Modal.Header></Modal.Header>
             <Modal.Body>
-                <form onSubmit={handleSumbit} className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
+                <form onSubmit={handleSumbit} className="space-y-6 px-6  sm:pb-6 lg:px-8 xl:pb-8">
                     <h3 className="text-xl font-medium text-gray-900 dark:text-white">
                         Contacta con nosotros
                     </h3>
@@ -60,6 +74,7 @@ export default function ModalContact(props: Prop) {
                             name="subject"
                             icon={HiAnnotation}
                             placeholder="Propuesta de negocio"
+                            required={true}
                             type="text" 
                         />                    
                     </div>
@@ -72,10 +87,28 @@ export default function ModalContact(props: Prop) {
                         </div>
                         <Textarea
                             id="comment"
+                            required={true}
                             name="message"
                             placeholder="Deja tu mensaje..."
                             rows={4}
                         />
+                    </div>
+                    <div>
+                    {success && (
+                        <Alert color="success">
+                            <span>
+                                <span className="font-medium">¡Exito!</span> Su correo ha sido mandado con exito.
+                            </span>
+                        </Alert>
+                    )}
+
+                    {error && (
+                        <Alert color="failure" icon={HiInformationCircle}>
+                            <span>
+                                <span className="font-medium">¡Error!</span> Parece que no se ha podido mandar intentalo de nuevo.
+                            </span>
+                        </Alert>
+                    )}
                     </div>
                     <div>
                         <Button type="submit">Enviar mensaje</Button>
