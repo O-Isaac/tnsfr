433ac71f6a805671e673ac08c9c54521dba438f3
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-05-19 22:16:16 +0200

    chore(layout): Change metatags close

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -16,20 +16,20 @@ export default function RootLayour(props: RootLayourProps) {
         <ContactModalContext.Provider value={{ setShow: setShowContact, show: showContact }}>
             <Head>
                 <title>The New Flex Studio Record</title>
-                <meta name="title" content="The New Flex Studio Record"></meta>
-                <meta name="description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas."></meta>
+                <meta name="title" content="The New Flex Studio Record" />
+                <meta name="description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas." />
 
-                <meta property="og:type" content="website"></meta>
-                <meta property="og:url" content="https://www.tnfsr.com/"></meta>
-                <meta property="og:title" content="The New Flex Studio Record"></meta>
-                <meta property="og:description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas."></meta>
-                <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:title" content="The New Flex Studio Record" />
+                <meta property="og:description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas." />
+                <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
 
-                <meta property="twitter:card" content="summary_large_image"></meta>
-                <meta property="twitter:url" content="https://www.tnfsr.com//"></meta>
-                <meta property="twitter:title" content="The New Flex Studio Record">
-                <meta property="twitter:description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas."></meta>
-                <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta></meta>
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:title" content="The New Flex Studio Record" />
+                <meta property="twitter:description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas." />
+                <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
             </Head>
             <Navigation />
             <main>
