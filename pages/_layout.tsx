db1e97f8f27279de377b0e8f4af0edd178cdc96c
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-05-19 22:19:10 +0200

    feat(layout): Change url & image

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -20,16 +20,16 @@ export default function RootLayour(props: RootLayourProps) {
                 <meta name="description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas." />
 
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://metatags.io/" />
+                <meta property="og:url" content="https://tnfsr.com" />
                 <meta property="og:title" content="The New Flex Studio Record" />
                 <meta property="og:description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas." />
-                <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+                <meta property="og:image" content="https://i.imgur.com/zVIhXfS.jpeg" />
 
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta property="twitter:url" content="https://tnfsr.com" />
                 <meta property="twitter:title" content="The New Flex Studio Record" />
                 <meta property="twitter:description" content="The New Flex Studio nace de la iniciativa de ayudar a la gente a producir sus ideas audiovisuales y graficas." />
-                <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+                <meta property="twitter:image" content="https://i.imgur.com/zVIhXfS.jpeg" />
             </Head>
             <Navigation />
             <main>
